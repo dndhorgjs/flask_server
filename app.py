eb08736cafde617d8e1cb3a99a7650fc7f474612
dndhorgjs <113087891+dndhorgjs@users.noreply.github.com>
2025-05-15 18:24:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template
+import pymysql
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html")  # templates/index.html 불러오기
+def index():
+    try:
+        conn = pymysql.connect(
+            host="metro.proxy.rlwy.net",
+            port=39083,  # 포트는 너 DB에 맞게
+            user="root",
+            password="CXJqQqOlYTACMQZPdSaNyvUeLtnfHVvH",
+            database="railway",
+            charset="utf8mb4"
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM wrist_log ORDER BY created_at DESC LIMIT 10")
+        logs = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return render_template("index.html", logs=logs)
+    except Exception as e:
+        return f"❌ DB 연결 실패: {e}"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
- 
